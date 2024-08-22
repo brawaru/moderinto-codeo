3e12717a88e98d718e44a8885849128e8b9a05da
Sasha Sorokin <10401817+brawaru@users.noreply.github.com>
2024-08-22 21:41:25 +0300

    Trigger 3

--- apps/frontend/src/app.vue
+++ apps/frontend/src/app.vue
@@ -6,6 +6,7 @@
   </NuxtLayout>
 </template>
 <script setup lang="ts">
+// bleh.
 import ModrinthLoadingIndicator from "~/components/ui/modrinth-loading-indicator.ts";
 import Notifications from "~/components/ui/Notifications.vue";
 </script>
